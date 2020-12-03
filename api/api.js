68f28791e7b9f81931132a1e61df627d00c1f3be
Pranav Teegavarapu <pranavnt@outlook.com>
2020-12-02 18:59:26 -0800

    :/

--- api/api.js
+++ api/api.js
@@ -1,21 +1,30 @@
 require("dotenv").config();
 
 const nodemailer = require("nodemailer");
+const app = require("express")();
 
-var transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: "youremail@address.com",
-    pass: "yourpassword",
-  },
+app.post("/send", function (req, res, next) {
+  var transporter = nodemailer.createTransport({
+    host: "localhost",
+    port: 587,
+    secure: false, // upgrade later with STARTTLS
+    auth: {
+      type: "login",
+      user: process.env.USER_EMAIL,
+      pass: process.env.USER_PASSWORD,
+    },
+  });
+
+  const mailOptions = {
+    from: process.env.FROM,
+    to: process.env.TO,
+    subject: "Testing",
+    html: "<p>Hi!</p>",
+  };
+  transporter.sendMail(mailOptions, function (err, info) {
+    if (err) console.log(err);
+    else console.log(info);
+  });
 });
-const mailOptions = {
-  from: "sender@email.com", // sender address
-  to: "to@email.com", // list of receivers
-  subject: "Subject of your email", // Subject line
-  html: "<p>Your html here</p>", // plain text body
-};
-transporter.sendMail(mailOptions, function (err, info) {
-  if (err) console.log(err);
-  else console.log(info);
-});
+
+app.listen(app.listen(3000, () => console.log(`Testing`)));
